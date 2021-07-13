45aeeedc5bd2174d9fbee1cb97a41d178ea9b647
Joshua Birk <joshua.birk@salesforce.com>
2021-07-13 16:36:30 -0500

    first

---
+++ index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+app.set('socketio', io);
+app.use(express.static(__dirname + '/public'));
+
+var token = null;
+
+var fs = require('fs'); 
+if (fs.existsSync('.handshake_token')){
+    token = fs.readFileSync('.handshake_token');
+}
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/img/:image/:token', function(req, res) {
+    if(token && req.params.token) {
+        if (token != req.params.token) {
+            res.json({"status":"bad token"});
+            return;
+        }
+    }
+    var io = req.app.get('socketio');
+    io.emit('image', { image: true, src: '/images/'+ req.params.image + '.gif'});
+    res.json({"status":"ok"});
+    console.log('served up image '+req.params.image);
+    });
+
