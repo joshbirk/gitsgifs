0bb2999a5d6d90ba1832756ce4e8672ce51c7dc2
Joshua Birk <joshua.birk@salesforce.com>
2021-07-13 17:16:30 -0500

    adding token code

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ app.use(express.static("public"))
 
 var token = process.env.HANDSHAKE_TOKEN;
 
-
 http.listen(process.env.PORT || 3000, function(){
   console.log('listening on *:3000');
 });
@@ -16,7 +15,13 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/img/:image', function(req, res) {
+app.get('/img/:image/:token', function(req, res) {
+    if(token && req.params.token) {
+        if (token != req.params.token) {
+            res.json({"status":"bad token"});
+            return;
+        }
+    }
     var io = req.app.get('socketio');
     io.emit('image', { image: true, src: '/images/'+ req.params.image + '.gif'});
     res.json({"status":"ok"});
